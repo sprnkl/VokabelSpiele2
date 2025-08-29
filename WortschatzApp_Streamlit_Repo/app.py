3f77d2e16d492ed6f315c9c17542e0449fbf0e2e
sprnkl <andreas_sprenkel@web.de>
2025-08-29 19:57:30 +0200

    Update app.py

--- WortschatzApp_Streamlit_Repo/app.py
+++ WortschatzApp_Streamlit_Repo/app.py
@@ -659,7 +659,7 @@ def game_irregulars_assign():
 
 def main():
     st.set_page_config(page_title="Wortschatz-Spiele (Klassen 7–9)", page_icon="📚", layout="wide")
-    st.title("Wortschatz-Spiele (Klassen 7–9) – Nur 'Diese Seite'")
+    st.title("Wortschatz-Spiele (Klassen 7–9) – Grundkurs")
 
     c_left, c_mid, c_debug = st.columns([2, 1, 1])
     with c_left:
